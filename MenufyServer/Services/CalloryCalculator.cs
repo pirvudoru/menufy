d89b75f049da311c193ad7b7f41345cc338b2775
pirvudoru <pirvu.doru@ymail.com>
2016-03-12 20:43:12 +0200

    add callory calculator placeholder

---
+++ MenufyServer/Services/CalloryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MenufyServer.Data;
+
+namespace MenufyServer.Services
+{
+    public class CalloryCalculator
+    {
+        public decimal CalculateNormalFor(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateCurrentFor(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
