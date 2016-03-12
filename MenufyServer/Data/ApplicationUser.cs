394b579f109a1c8d202f918d76febec2d0e8f0af
DaniBis <bisceanudaniel@gmail.com>
2016-03-12 18:18:25 +0200

    Am facut viewuri pentru Settings,Menu,Recipe,setup registration

--- MenufyServer/Data/ApplicationUser.cs
+++ MenufyServer/Data/ApplicationUser.cs
@@ -8,6 +8,12 @@ namespace MenufyServer.Data
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string Birthdate { get; internal set; }
+        public string Gender { get; internal set; }
+        public string Height { get; internal set; }
+        public string Lifestyle { get; internal set; }
+        public string Weight { get; internal set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
--- MenufyServer/ViewModels/RegisterViewModel.cs
+++ MenufyServer/ViewModels/RegisterViewModel.cs
@@ -10,6 +10,26 @@ namespace MenufyServer.ViewModels
         public string Email { get; set; }
 
         [Required]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "BirthDate")]
+        public string BirthDate { get; set; }
+
+        [Required]
+        [Display(Name = "Weight")]
+        public string Weight { get; set; }
+
+        [Required]
+        [Display(Name = "Height")]
+        public string Height { get; set; }
+
+        [Required]
+        [Display(Name = "Lifestyle")]
+        public string Lifestyle { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
