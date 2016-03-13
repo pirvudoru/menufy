97c75df38fcb45731907b529825729d38aa138dd
Catrina Marius <mcatrina14@gmail.com>
2016-03-13 04:04:12 +0200

    link to recipe

--- MenufyServer/Controllers/RecipeController.cs
+++ MenufyServer/Controllers/RecipeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MenufyServer.Data;
 
 namespace MenufyServer.Controllers
 {
+    [Route("/[controller]")]
     public class RecipeController : Controller
     {
         // GET: Recipe
-        public ActionResult Index()
+        [System.Web.Mvc.HttpGet]
+        [Route("/Details/{id}")]
+        public ActionResult Details(int id)
         {
+            var context = ApplicationDbContext.Create();
+            Recipe recipe = (context.Recipes.Take(id).ToList()).First(r => r.Id == id);
+            ViewBag.photoUrl = recipe.PhotoUrl;
+            ViewBag.ingredients = recipe.Ingredients;
+            ViewBag.steps = recipe.Steps;
+            ViewBag.name = recipe.Title;
             return View();
         }
     }
